65f95f1324b0514d814877750bc01209371bd32f
Kkeie <espitsyn74@gmail.com>
2024-04-25 18:40:03 +0500

    Added SplahTower

---
+++ Assets/Scripts/SplashBullet.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashBulletMovement : MonoBehaviour
+{
+    [SerializeField] private float bulletSpeed = 0.5f;
+    [SerializeField] private float bulletLifeTime = 3; 
+    private CircleCollider2D collider;
+    private bool IsStoped = false;
+    private List<GameObject> injured;
+    void Start()
+    {
+        collider = GetComponent<CircleCollider2D>();
+        injured = new();
+    }
+
+    void Update()
+    {
+        if (!IsStoped)
+            transform.Translate(Vector3.up * bulletSpeed);
+        CheckFall();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            collider.radius = 30;
+            StartCoroutine(DeleteBullet());
+            IsStoped = true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {  
+        if (collision.tag == "Enemy" && !injured.Contains(collision.gameObject)) 
+        {
+                collision.GetComponent<BasicEnemy>().Health--;
+                injured.Add(collision.gameObject);
+        }
+    }
+
+    private IEnumerator DeleteBullet()
+    {
+        yield return new WaitForSeconds(0.05f);
+        Destroy(gameObject);
+    }
+
+    private void CheckFall()
+    {
+        bulletLifeTime -= Time.deltaTime;
+        if (bulletLifeTime <= 0)
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/SplashTower.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Serialization;
+using UnityEngine;
+public class SplashTower : MonoBehaviour
+{
+    [SerializeField]
+    GameObject bullet;
+    [SerializeField]
+    private float fireRate = 1f;
+    private float currentCooldown;
+    private bool isLocked;
+    private Vector2 vectorToTarget;
+    private float radius = 3;
+    
+    void Start()
+    {
+        currentCooldown = fireRate;
+    }
+
+
+    void Update()
+    {
+        LookAtNearestEnemy();
+        if (isLocked)
+            Shoot();
+    }
+
+    private void LookAtNearestEnemy()
+    {
+        vectorToTarget = FindNeatestEnemy();
+        float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle - 90);
+    }
+
+    private Vector2 FindNeatestEnemy()
+    {
+        float shortestDistance = Mathf.Infinity;
+        var vector = Vector2.zero;
+        foreach (var enemy in MapGlobalFields.allEnemies)
+        {
+            if (Vector2.Distance(enemy.transform.position, transform.position) < shortestDistance)
+            {
+                shortestDistance = Vector2.Distance(enemy.transform.position, transform.position);
+                vector = enemy.transform.position - transform.position;
+            }
+        }
+        isLocked = vector != Vector2.zero;
+        return vector;
+    }
+
+    private void Shoot()
+    {
+        
+        currentCooldown -= Time.deltaTime;
+        if (currentCooldown <= 0)
+        {
+            Instantiate(bullet, transform.position + new Vector3(vectorToTarget.x, vectorToTarget.y, 0).normalized, transform.rotation);
+            currentCooldown = fireRate;
+        }
+    }
+}
