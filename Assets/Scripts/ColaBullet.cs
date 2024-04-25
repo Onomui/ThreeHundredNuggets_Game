09f522e5b536cc547eb4438657b110445de87340
Дмитрий <145160988+lyubdim@users.noreply.github.com>
2024-04-25 19:19:18 +0500

    Added Frozen Tower

---
+++ Assets/Scripts/ColaBullet.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColaBullet : MonoBehaviour
+{
+    [SerializeField]
+    private float bulletSpeed = 0.5f;
+    [SerializeField]
+    private float bulletLifeTime = 3;
+    [SerializeField]
+    private float stopMovement = 2f;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        transform.Translate(Vector3.up * bulletSpeed);
+        CheckFall();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+    if (collision.tag == "Enemy")
+        {
+            var enemy = collision.GetComponent<BasicEnemy>();
+            enemy.StopMovement(stopMovement);
+            enemy.ChangeSprite();
+            Destroy(gameObject);
+        }
+    }
+    
+    private void CheckFall()
+    {
+        bulletLifeTime -= Time.deltaTime;
+        if (bulletLifeTime <= 0)
+            Destroy(gameObject);
+    }
+
+}
