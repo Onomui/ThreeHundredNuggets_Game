4e16b85131c85cd1cddfb254bcec2cb0a2ae32b3
Robert Malikov <Robert.Malikov@urfu.me>
2024-05-02 23:36:37 +0500

    popcorn

---
+++ Assets/Scripts/PopcornTower.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Serialization;
+using UnityEngine;
+public class PopcornTower : MonoBehaviour
+{
+    [SerializeField]
+    GameObject bullet;
+    [SerializeField]
+    private float fireRate = 1f;
+    private float currentCooldown;
+    private bool isLocked;
+    private Vector2 vectorToTarget;
+    private float radius = 7;
+    void Start()
+    {
+        currentCooldown = fireRate;
+    }
+
+
+    void Update()
+    {       
+        Shoot();
+    }
+
+    private void Shoot()
+    {
+        currentCooldown -= Time.deltaTime;
+        if (currentCooldown <= 0)
+        {
+            Instantiate(bullet, transform.position + new Vector3(1, 0, 0), Quaternion.Euler(new Vector3(0, 0, -90)));
+            Instantiate(bullet, transform.position + new Vector3(0, 1, 0), Quaternion.Euler(new Vector3(0, 0, 0)));
+            Instantiate(bullet, transform.position + new Vector3(-1, 0, 0), Quaternion.Euler(new Vector3(0, 0, 90)));
+            Instantiate(bullet, transform.position + new Vector3(0, -1, 0), Quaternion.Euler(new Vector3(0, 0, 180)));
+
+            currentCooldown = fireRate;
+        }
+    }
+}
