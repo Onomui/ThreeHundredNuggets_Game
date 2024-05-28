49980147c24bd8dd68659e296b8b7f1cafe4d11c
Robert Malikov <Robert.Malikov@urfu.me>
2024-05-29 01:09:41 +0500

    main menu

--- Assets/Scripts/UI/TutorialScript.cs
+++ Assets/Scripts/UI/TutorialScript.cs
@@ -2,11 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class TutorialScript : MonoBehaviour
 {
+    public Sprite Health;
+    public Sprite Enemies;
+    public Sprite Money;
+    public Sprite Towers;
+    private VisualElement bg;
+    private Sprite[] slides;
+
+    private int i = 1;
+    private void Awake()
+    {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+
+        bg = root.Q<VisualElement>("Bg");
+        slides = new Sprite[] {Health, Enemies, Money, Towers};
+    }
     private void OnMouseDown()
     {
-        Camera.main.GetComponent<UiUpdateManager>().ChangeScreen("unTutor");
+        if (i <= 3)
+        {
+            bg.style.backgroundImage = new StyleBackground(slides[i]);
+            i++;
+        }
+        else
+        {
+            Camera.main.GetComponent<UiUpdateManager>().ChangeScreen("unTutor");
+        }
     }
 }
